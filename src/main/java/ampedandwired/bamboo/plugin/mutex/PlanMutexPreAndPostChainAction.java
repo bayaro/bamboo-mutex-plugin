f43a5d3da6d0c1248b95c947306c03bd7e4998bf
Peter Broere <peter.broere@prodrive-technologies.com>
2018-06-21 17:04:20 +0200

    only lock the plan using the ConcurrentHashMap

--- src/main/java/ampedandwired/bamboo/plugin/mutex/PlanMutexPreAndPostChainAction.java
+++ src/main/java/ampedandwired/bamboo/plugin/mutex/PlanMutexPreAndPostChainAction.java
@@ -13,6 +13,7 @@ import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -21,27 +22,24 @@ public class PlanMutexPreAndPostChainAction implements PreChainAction, PostChain
     public static final String PLAN_MUTEX_KEY = "custom.bamboo.planMutex.list";
     private PlanManager planManager;
 
-    private static ConcurrentMap<String, PlanKey> runningPlans = new ConcurrentHashMap<String, PlanKey>();
-
+    private static ConcurrentMap<String, UUID> runningPlans = new ConcurrentHashMap<String, UUID>();
+    
     @Override
     public void execute(@NotNull Chain chain, @NotNull ChainExecution chainExecution) throws Exception {
         PlanKey thisPlanKey = PlanKeys.getPlanKey(chain.getKey());
+        UUID uuid = java.util.UUID.randomUUID();
+        
         Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
         String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
+        
         if (planMutexKey != null && !planMutexKey.trim().isEmpty()) {
-            PlanKey planToWaitFor = runningPlans.putIfAbsent(planMutexKey, thisPlanKey);
-            while (planToWaitFor != null) {
-                log.info("Waiting for plan " + planToWaitFor + " holding mutex '" + planMutexKey + "' to complete before executing " + thisPlanKey);
-                while (isPlanCurrentlyExecuting(planToWaitFor)) {
-                    Thread.sleep(1000);
-                }
-                if (runningPlans.replace(planMutexKey, planToWaitFor, thisPlanKey)) {
-                    planToWaitFor = null;
-                } else {
-                    planToWaitFor = runningPlans.putIfAbsent(planMutexKey, thisPlanKey);
-                }
-            }
-            log.info("No plans with mutex '" + planMutexKey + "' are currently building. Proceeding with build of " + thisPlanKey);
+          while(runningPlans.putIfAbsent(planMutexKey, uuid) != uuid)
+          {
+            log.info("Still waiting for mutex '" + planMutexKey + "' with plan " + thisPlanKey);
+            Thread.sleep(1000);
+          }
+          
+          log.info("Locked mutex '" + planMutexKey + "' with plan " + thisPlanKey);
         }
     }
 
@@ -50,20 +48,10 @@ public class PlanMutexPreAndPostChainAction implements PreChainAction, PostChain
         PlanKey thisPlanKey = PlanKeys.getPlanKey(chain.getKey());
         Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
         String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
+        
         if (planMutexKey != null && !planMutexKey.trim().isEmpty()) {
-            runningPlans.remove(planMutexKey, thisPlanKey);
-            log.info("Released mutex '" + planMutexKey + "' for build of " + thisPlanKey);
+          runningPlans.remove(planMutexKey);
+          log.info("Released mutex '" + planMutexKey + "' with plan " + thisPlanKey);
         }
-    }
-
-    private boolean isPlanCurrentlyExecuting(PlanKey planKey) {
-        Plan plan = planManager.getPlanByKey(planKey);
-        if (plan == null)
-          return false;
-        return plan.isExecuting();
-    }
-
-    public void setPlanManager(PlanManager planManager) {
-        this.planManager = planManager;
     }
 }
