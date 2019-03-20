636d63d45e54a323e9a147789472d2f288901287
baya <baya@andatra.kiev.ua>
2019-03-20 17:19:47 +0200

    + PlanMutexProcessor class

--- src/main/java/ampedandwired/bamboo/plugin/mutex/PlanMutexPreAndPostChainAction.java
+++ src/main/java/ampedandwired/bamboo/plugin/mutex/PlanMutexPreAndPostChainAction.java
@@ -1,69 +1,45 @@
 package ampedandwired.bamboo.plugin.mutex;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.atlassian.bamboo.chains.Chain;
 import com.atlassian.bamboo.chains.ChainExecution;
 import com.atlassian.bamboo.chains.ChainResultsSummary;
 import com.atlassian.bamboo.chains.plugins.PostChainAction;
 import com.atlassian.bamboo.chains.plugins.PreChainAction;
-import com.atlassian.bamboo.plan.Plan;
-import com.atlassian.bamboo.plan.PlanKey;
-import com.atlassian.bamboo.plan.PlanKeys;
-import com.atlassian.bamboo.plan.PlanManager;
-import com.atlassian.bamboo.builder.LifeCycleState;
-import org.apache.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class PlanMutexPreAndPostChainAction implements PreChainAction, PostChainAction {
-  public static final Logger log = Logger.getLogger(PlanMutexPreAndPostChainAction.class);
-  public static final String PLAN_MUTEX_KEY = "custom.bamboo.planMutex.list";
-  private PlanManager planManager;
+    public static final String PLAN_MUTEX_KEY = "custom.bamboo.planMutex.list";
 
-  private static ConcurrentMap<String, String> runningPlans = new ConcurrentHashMap<String, String>();
+    private static PlanMutexProcessor pmProcessor = PlanMutexProcessor.getInstance();
 
-  @Override
-  public void execute(@NotNull Chain chain, @NotNull ChainExecution chainExecution) throws Exception {
-    String id = chainExecution.getPlanResultKey().toString();
-    Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
-    String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
+    @NotNull
+    @Override
+    public void execute(@NotNull Chain chain, @NotNull ChainExecution chainExecution) {
+         String id = chain.getPlanKey().toString();
+         Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
+         String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
 
-    if (planMutexKey != null && !planMutexKey.trim().isEmpty())
-    {
-      log.info("Starting to wait for mutex '" + planMutexKey + "' with id " + id);
-      while (runningPlans.putIfAbsent(planMutexKey, id) != id) {
-        Thread.sleep(1000);
-      }
-
-      // we have the mutex, check if we are still in running state
-      if(chainExecution.isStopping() || chainExecution.isStopRequested())
-      {
-        log.info("Released mutex '" + planMutexKey + "' with id " + id + " since we are already stopped");
-        runningPlans.remove(planMutexKey, id);
-      }
-      else
-      {
-        log.info("Locked mutex '" + planMutexKey + "' with id " + id);
-      }
+         if (planMutexKey != null && !planMutexKey.trim().isEmpty()) {
+              pmProcessor.lockMutex( planMutexKey, id );
+              // we have the mutex, check if we are still in running state
+              if(chainExecution.isStopping() || chainExecution.isStopRequested()) {
+                   pmProcessor.releaseMutex( planMutexKey, id );
+              }
+         }
     }
-  }
 
-  @Override
-  public void execute(Chain chain, ChainResultsSummary chainResultsSummary, ChainExecution chainExecution)
-      throws InterruptedException, Exception {
-    String id = chainExecution.getPlanResultKey().toString();
-    Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
-    String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
+    @Override
+    public void execute(Chain chain, ChainResultsSummary chainResultsSummary, ChainExecution chainExecution) {
+        String id = chain.getPlanKey().toString();
+        Map<String, String> customConfig = chain.getBuildDefinition().getCustomConfiguration();
+        String planMutexKey = customConfig.get(PLAN_MUTEX_KEY);
 
-    if (planMutexKey != null && !planMutexKey.trim().isEmpty()) {
-      if (runningPlans.remove(planMutexKey, id)) {
-        log.info("Released mutex '" + planMutexKey + "' with id " + id);
-      } else {
-        log.error("Did not release mutex '" + planMutexKey + "' with id " + id + " since it was claimed by " + runningPlans.get(planMutexKey));
-      }
+        if (planMutexKey != null && !planMutexKey.trim().isEmpty()) {
+            pmProcessor.releaseMutex( planMutexKey, id );
+        }
     }
-  }
 }
---
+++ src/main/java/ampedandwired/bamboo/plugin/mutex/PlanMutexProcessor.java
@@ -0,0 +1,95 @@
+package ampedandwired.bamboo.plugin.mutex;
+
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public final class PlanMutexProcessor {
+    private static final Logger log = Logger.getLogger(PlanMutexProcessor.class);
+    private static ConcurrentMap<String, String> runningPlans = new ConcurrentHashMap<String, String>();
+    private static Map<String, Long> lockOrder = new HashMap<String, Long>();
+    public static Map<String, String> deploymentMutexes = new HashMap<String, String>();
+
+    public static volatile PlanMutexProcessor _instance = null;
+
+    public PlanMutexProcessor() {}
+
+    public static synchronized PlanMutexProcessor getInstance() {
+        if (_instance == null)
+            synchronized (PlanMutexProcessor.class) {
+                if (_instance == null)
+                    _instance = new PlanMutexProcessor();
+            }
+        return _instance;
+    }
+
+    public void lockMutex( String planMutexKey, String id ) {
+        log.info( ":::::: " + runningPlans.toString() );
+        String msg;
+        String lockedBy = runningPlans.get( planMutexKey );
+        if ( lockedBy != null ) {
+            if ( lockedBy.equals( id ) ) {
+                msg = "====== Mutex '" + planMutexKey + "' already locked by " + lockedBy;
+                log.info( msg );
+                return;
+            }
+            msg = "====== Waiting for mutex '" + planMutexKey + "' locked by " + lockedBy;
+            log.info( msg );
+        }
+        int i = 0;
+        long myTime = System.currentTimeMillis();
+        boolean imFirst = true;
+        lockOrder.putIfAbsent( id, myTime );
+        while ( true ) {
+            if ( i % 10 == 0 ) {
+                log.info( "****** " + i + " " + planMutexKey + ":" + id + " >>> " + runningPlans.toString() + " >>> " + lockOrder.toString() );
+            }
+            i++;
+            if ( i > 3600 ) return; // 1 hour
+
+            Iterator it = lockOrder.entrySet().iterator();
+            imFirst = true;
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry)it.next();
+                if ( (Long)pair.getValue() < myTime ) {
+                    imFirst = false;
+                    break;
+                }
+            }
+            if ( imFirst && ( runningPlans.putIfAbsent(planMutexKey, id) == id) ) {
+                break;
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                return;
+            }
+        }
+        msg = "====== Mutex '" + planMutexKey + "' was locked with id " + id;
+        log.info( msg );
+    }
+
+    public void releaseMutex( String planMutexKey, String id ) {
+        log.info( ":::::: " + runningPlans.toString() );
+        String msg;
+        if (runningPlans.remove(planMutexKey, id)) {
+            msg = "====== Mutex '" + planMutexKey + "' released with id " + id;
+            log.info( msg );
+        } else {
+            msg = "====== Mutex '" + planMutexKey + "' did not released with id " + id + " since it was claimed by " + runningPlans.get(planMutexKey);
+            log.error( msg );
+            //runningPlans.remove(planMutexKey, runningPlans.get(planMutexKey) );
+            //msg = "=!=!=!= Mutex '" + planMutexKey + "' released with id " + runningPlans.get(planMutexKey);
+        }
+        lockOrder.remove( id );
+    }
+
+    public String get( String planMutexKey ) {
+        return runningPlans.get( planMutexKey );
+    }
+}
